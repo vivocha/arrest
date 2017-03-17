9ca5621e47793cf900d8fba64d4bd035ea647431
Federico Pinna <fpinna@vivocha.com>
2017-03-17 10:48:35 +0100

    feat(module): Add missing export of the Operation class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { RESTError } from './error';
 export { SchemaRegistry } from './schema';
 export { API } from './api';
 export { Resource } from './resource';
+export { Operation } from './operation';
 export * from './mongo';
